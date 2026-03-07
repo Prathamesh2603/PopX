391e7ca78582d124db3e0e9c1ac182b742e7f1e3
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-03-07 21:58:46 +0530

    for creating the create account page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { CreateAccPage } from "./pages/CreateAccPage";
 import { WelcomePage } from "./pages/WelcomePage";
 
 
 const App = () => {
   return (
     <div className="bg-black h-screen w-full py-4">
-      <WelcomePage />      
+      {/* <WelcomePage />       */}
+      <CreateAccPage />
     </div>
   )
 }

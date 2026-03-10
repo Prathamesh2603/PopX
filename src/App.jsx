6f539892182a8ca7b795ecf84b4a5ff68fbe7a7e
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-03-10 10:45:14 +0530

    routes are added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { AccountSetting } from "./pages/AccountSetting";
 import { CreateAccPage } from "./pages/CreateAccPage";
 import { LoginPage } from "./pages/LoginPage";
 import { WelcomePage } from "./pages/WelcomePage";
-
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="bg-black h-screen w-full py-4">
-      {/* <WelcomePage />       */}
-      {/* <CreateAccPage /> */}
-      {/* <LoginPage /> */}
-      <AccountSetting />
+      <Routes>
+        <Route path="/" element={ <WelcomePage /> } />
+        <Route path="/account" element={ <CreateAccPage /> } />
+        <Route path="/login" element={ <LoginPage /> } />
+        <Route path="/setting" element={ <AccountSetting /> } />
+      </Routes>
     </div>
   )
 }
